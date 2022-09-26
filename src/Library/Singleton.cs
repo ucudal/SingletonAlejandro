8c1890045d1dcf572f9fb438ef1a24b11c406ec8
alejandropiccardo <alejandro.piccardo@correo.ucu.edu.uy>
2022-09-26 10:25:39 -0300

    finish

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Library;
+
+public class  Singleton<T> where T : new()
+{
+    private Singleton()
+    {
+
+    }
+    private static T singletonInstance;
+    public static T SingletonInstance
+    {
+        get
+        {
+            if (singletonInstance == null)
+            {
+                singletonInstance = new T();
+            }
+
+            return singletonInstance;
+        }
+    }
+
+}
